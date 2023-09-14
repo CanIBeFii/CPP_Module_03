226de2fec6411329a1ce5be8b7924cfd275c5b70
CanIBeFii <filipe250502@gmail.com>
2023-09-14 15:15:20 +0100

    finish ex00

---
+++ ex00/sources/main.cpp
@@ -0,0 +1,28 @@
+#include "ClapTrap.hpp"
+
+int	main( void ) {
+	ClapTrap	bob( "bob" );
+
+	std::cout << bob.getName() << " has " << bob.getHitPoints() << " hit points ";
+	std::cout << bob.getEnergyPoints() << " energy points and ";
+	std::cout << bob.getAttackDamage() << " attack damage" << std::endl;
+
+	bob.attack( "target" );
+
+	bob.takeDamage( 5 );
+
+	std::cout << bob.getName() << " has " << bob.getHitPoints() << " hit points" << std::endl;
+
+	bob.beRepaired( 10 );
+
+	bob.attack( "target" );
+
+	bob.takeDamage( 100 );
+
+	bob.beRepaired( 100 );
+
+	std::cout << bob.getName() << " has " << bob.getHitPoints() << " hit points ";
+	std::cout << bob.getEnergyPoints() << " energy points" << std::endl;
+
+	return ( 0 );
+}
