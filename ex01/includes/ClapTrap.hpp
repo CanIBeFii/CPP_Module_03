3cfe813ed44712425f92905df62db757ecc309f6
CanIBeFii <filipe250502@gmail.com>
2023-09-14 17:27:10 +0100

    ex01: add initial files and ScavTrap

---
+++ ex01/includes/ClapTrap.hpp
@@ -0,0 +1,38 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+class ClapTrap {
+	public:
+		// Othodox Canonical Form
+		ClapTrap( void );
+		ClapTrap( std::string name );
+		ClapTrap( const ClapTrap& copy );
+		ClapTrap&	operator=( const ClapTrap& copy );
+		~ClapTrap( void );
+
+		// Getters
+		std::string		getName( void ) const;
+		unsigned int	getHitPoints( void ) const;
+		unsigned int	getEnergyPoints( void ) const;
+		unsigned int	getAttackDamage( void ) const;
+
+		// Setters
+		void		setName( std::string newName );
+		void		setHitPoints( unsigned int newHitPoints );
+		void		setEnergyPoints( unsigned int newEnergyPoints );
+		void		setAttackDamage( unsigned int newAttackDamage );
+		
+		// Functions
+		void		attack( const std::string& target );
+		void		takeDamage( unsigned int amount );
+		void		beRepaired( unsigned int amount );
+
+	protected:
+		std::string		_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
+};
+
+#endif
---
+++ ex01/includes/ScavTrap.hpp
@@ -0,0 +1,21 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		// Othodox Canonical Form
+		ScavTrap( void );
+		ScavTrap( std::string name );
+		ScavTrap( const ScavTrap& copy );
+		ScavTrap&	operator=( const ScavTrap& copy );
+		~ScavTrap( void );
+
+		// Functions
+		void		attack( const std::string& target );
+		void		guardGate( void );
+};
+
+#endif
